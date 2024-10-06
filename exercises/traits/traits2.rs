b853a00fa6c305ccff3aa84a1aee980f1afd325d
userName <userEmail>
2024-10-06 22:48:24 +0800

    solve traits1,2,34

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,26 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        let mut result = Vec::new();
+        for s in self {
+            result.push(s);
+            result.push(String::from("Bar"));
+        }
+        result
+    }
+}
+
+
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+
 
 #[cfg(test)]
 mod tests {
